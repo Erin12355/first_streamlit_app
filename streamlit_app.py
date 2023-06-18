3b85a577de4e6c0daf6bb1feba38e1b7106567d8
Jaeson Laishram <87673700+Erin12355@users.noreply.github.com>
2023-06-18 23:11:47 +0530

    Update streamlit_app.py
    streamlit spelling correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title("My Parent New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
-Treamlit.text("Hard-boiled Free-Range Egg")
+streamlit.text("Hard-boiled Free-Range Egg")
               
